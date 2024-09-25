4457c666c918632d75952a5b2504ebd9fb716f18
sangwoo <lesw1216@gmail.com>
2024-09-25 19:54:31 +0900

    Initial commit

---
+++ src/main/java/com/sw/starcraft/StarcraftApplication.java
@@ -0,0 +1,13 @@
+package com.sw.starcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarcraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sw/starcraft/StarcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.starcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
